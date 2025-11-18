4cb1e5779e71f607dbe55d22bee3813d6327467a
r-alvarez <ralvarez_valentini@hotmail.com>
2025-11-18 21:57:41 +0000

    Implemented red tests for both BuyOneGetOneFree strategy and Percentage Discount Strategy Tests

---
+++ tests/CheckoutKata.Application.Tests/Pricing/BuyOneGetOneFreeStrategyTests.cs
@@ -0,0 +1,49 @@
+﻿using CheckoutKata.Application.Pricing.Strategies;
+using FluentAssertions;
+
+namespace CheckoutKata.Application.Tests.Pricing;
+
+public class BuyOneGetOneFreeStrategyTests
+{
+    private readonly BuyOneGetOneFreeStrategy _strategy = new();
+
+    [Fact]
+    public void CalculatePrice_WhenOneItem_ReturnsUnitPrice()
+    {
+        var price = _strategy.CalculatePrice(1, 50);
+
+        price.Should().Be(50);
+    }
+
+    [Fact]
+    public void CalculatePrice_WhenTwoItems_ReturnsOneUnitPrice()
+    {
+        var price = _strategy.CalculatePrice(2, 50, 2, 0);
+
+        price.Should().Be(50, because: "buy 2, pay for 1");
+    }
+
+    [Fact]
+    public void CalculatePrice_WhenThreeItems_ReturnsTwoUnitPrices()
+    {
+        var price = _strategy.CalculatePrice(3, 50, 2, 0);
+
+        price.Should().Be(100, because: "buy 3, pay for 2");
+    }
+
+    [Fact]
+    public void CalculatePrice_WhenFourItems_ReturnsTwoUnitPrices()
+    {
+        var price = _strategy.CalculatePrice(4, 50, 2, 0);
+
+        price.Should().Be(100, because: "buy 4, pay for 2");
+    }
+
+    [Fact]
+    public void CalculatePrice_WhenFiveItems_ReturnsThreeUnitPrices()
+    {
+        var price = _strategy.CalculatePrice(5, 50, 2, 0);
+
+        price.Should().Be(150, because: "buy 5, pay for 3");
+    }
+}
---
+++ tests/CheckoutKata.Application.Tests/Pricing/PercentageDiscountStrategyTests.cs
@@ -0,0 +1,41 @@
+﻿using CheckoutKata.Application.Pricing.Strategies;
+using FluentAssertions;
+
+namespace CheckoutKata.Application.Tests.Pricing;
+
+public class PercentageDiscountStrategyTests
+{
+    private readonly PercentageDiscountStrategy _strategy = new();
+
+    [Fact]
+    public void CalculatePrice_WhenBelowThreshold_ReturnsFullPrice()
+    {
+        var price = _strategy.CalculatePrice(2, 50, specialQuantity: 3, specialPrice: 10);
+
+        price.Should().Be(100, because: "need 3+ items for discount");
+    }
+
+    [Fact]
+    public void CalculatePrice_WhenAtThreshold_AppliesDiscount()
+    {
+        var price = _strategy.CalculatePrice(3, 50, specialQuantity: 3, specialPrice: 10);
+
+        price.Should().Be(135, because: "3 × 50 = 150, 10% off = 135");
+    }
+
+    [Fact]
+    public void CalculatePrice_WhenAboveThreshold_AppliesDiscount()
+    {
+        var price = _strategy.CalculatePrice(5, 50, specialQuantity: 3, specialPrice: 20);
+
+        price.Should().Be(200, because: "5 × 50 = 250, 20% off = 200");
+    }
+
+    [Fact]
+    public void CalculatePrice_WhenNoSpecialQuantity_ReturnsFullPrice()
+    {
+        var price = _strategy.CalculatePrice(5, 50);
+
+        price.Should().Be(250);
+    }
+}
