5ced8927e1c5aeccd235c122f635bd06d471450a
r-alvarez <ralvarez_valentini@hotmail.com>
2025-11-18 03:56:18 +0000

    Initialized project, with first iteration of Checkout Scan and GetTotalPrice Red tests, this code doesn't compile as expected given the missing implementation of both ICheckout contract and PricingRule engine

---
+++ tests/CheckoutKata.Application.Tests/CheckoutTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+
+namespace CheckoutKata.Application.Tests;
+
+public class CheckoutTests
+{
+    [Fact]
+    public void Scan_WhenCalled_DoesNotThrow()
+    {
+        var rules = Array.Empty<PricingRule>();
+        ICheckout checkout = CreateCheckout(rules);
+
+        var act = () => checkout.Scan("A");
+
+        act.Should().NotThrow();
+    }
+
+    [Fact]
+    public void GetTotalPrice_WhenNoItemsScanned_ReturnsZero()
+    {
+        var rules = Array.Empty<PricingRule>();
+        ICheckout checkout = CreateCheckout(rules);
+
+        var total = checkout.GetTotalPrice();
+
+        total.Should().Be(0);
+    }
+
+    private static ICheckout CreateCheckout(IEnumerable<PricingRule> pricingRules)
+    {
+        return new Checkout(pricingRules);
+    }
+}
